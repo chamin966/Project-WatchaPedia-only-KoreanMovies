1b07b5ca8b2cba0303ce3262d7a7ebfbeca0c438
minchan <chamin966@naver.com>
2022-11-06 16:26:58 +0900

    Design: Header 컴포넌트 css 작업 완료
    샘플용 이미지 추가, common.css 생성, font 추가

--- my-project/src/components/Header/Header.jsx
+++ my-project/src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
+import './Header.css';
+import '../../css/common.css';
+
 function Header() {
   return (
     <div>
-      <div>Header</div>
+      <header className='header'>
+        <div className='header__left'>
+          <img
+            className='header__logo'
+            src='/images/samples/watchapediaLogo.png'
+            alt='왓챠피디아 로고'
+            height={'30px'}
+            width={'150px'}
+          />
+          <button className='header__title btn--none btn gray-word'>영화</button>
+        </div>
+        <div className='header__right'>
+          <form action='get'>
+            <input className='header__search-bar' type='text' placeholder='영화를 검색해보세요.' />
+          </form>
+          <button className='header__btn--login btn--none btn gray-word'>로그인</button>
+          <button className='header__btn--signup btn'>회원가입</button>
+        </div>
+      </header>
     </div>
   );
 }
