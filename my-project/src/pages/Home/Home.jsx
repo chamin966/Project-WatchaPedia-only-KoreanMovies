e38ed6a1f85b4269aacdde97a71362bb0ca65142
minchan <chamin966@naver.com>
2022-11-11 00:35:12 +0900

    Design: 홈 화면 이미지 슬라이드 CSS 완성
    일부러 여러 개 넣어놓은 것들은 api연동 시에 모듈화 시킬 것임

--- my-project/src/pages/Home/Home.jsx
+++ my-project/src/pages/Home/Home.jsx
@@ -1,182 +1,211 @@
+import { useRef, useState } from 'react';
 import './Home.css';
 
 function Home() {
+  const [move, setMove] = useState(0);
+  const leftClick = () => {
+    setMove(0);
+  };
+  const rightClick = () => {
+    setMove(-1125);
+  };
+
   return (
     <main className='home'>
-      <div className='home__lanking__content'>
-        <h2>박스오피스 순위</h2>
-        <div className='home__lanking'>
-          <div className='home__left-stick'>
-            <div className='home__left-chevron'>
-              <img src='/images/icons/left-chevron.png' alt='앞프로' height={'25px'} width={'25px'} />
+      <h2>박스오피스 순위</h2>
+      <div className='home__lanking'>
+        <div className='home__left-stick'>
+          {move === 0 ? (
+            <></>
+          ) : (
+            <div className='home__left-chevron' onClick={leftClick}>
+              <img src='/images/icons/left-chevron.png' alt='앞프로' height={'30px'} width={'30px'} />
             </div>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__lanked-movies'>
-            <div className='home__lanked-movies__border'>
-              <img
-                className='home__lanked-movies__poster'
-                src='images/samples/boxOfficePoster.jpg'
-                alt=''
-                height={'288px'}
-                width={'198px'}
-              />
-            </div>
-            <ul>
-              <li>더 랍스터</li>
-              <li>2015・아일랜드/영국/그리스</li>
-              <li className='gray-word-s'>예매율 25%</li>
-            </ul>
-          </div>
-          <div className='home__right-stick'>
-            <div className='home__right-chevron'>
-              <img src='/images/icons/right-chevron.png' alt='앞프로' height={'25px'} width={'25px'} />
-            </div>
-          </div>
+          )}
         </div>
+        <div className='home__lanking__container'>
+          <ul className='home__lanking_contents' style={{ transform: `translate(${move}px)` }}>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>1</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>2</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>3</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>4</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>5</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>6</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>7</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>8</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>9</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+            <ul className='home__lanked-movies'>
+              <li className='lanked-movies__numbering'>10</li>
+              <li className='home__lanked-movies__border'>
+                <img
+                  className='home__lanked-movies__poster'
+                  src='images/samples/boxOfficePoster.jpg'
+                  alt=''
+                  height={'288px'}
+                  width={'198px'}
+                />
+              </li>
+              <ul>
+                <li>더 랍스터</li>
+                <li>2015・아일랜드/영국/그리스</li>
+                <li className='gray-word-s'>예매율 25%</li>
+              </ul>
+            </ul>
+          </ul>
+        </div>
+        {move === -1125 ? (
+          <></>
+        ) : (
+          <div className='home__right-stick'>
+            <div className='home__right-chevron' onClick={rightClick}>
+              <img src='/images/icons/right-chevron.png' alt='앞프로' height={'30px'} width={'30px'} />
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
